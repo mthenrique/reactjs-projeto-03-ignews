6840f49acf4c951ebefa955991461324ac28b0c1
Mateus oliveira <mthenriqueo@gmail.com>
2022-05-13 19:40:44 -0300

    implements home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import Head from "next/head"
+import { SubscribeButton } from "./components/SubscribeButton"
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
-      <Head>Inicio | ig.news</Head>
-      <h1 className="">Hello world</h1>
+      <Head>Home | ig.news</Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.99 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
