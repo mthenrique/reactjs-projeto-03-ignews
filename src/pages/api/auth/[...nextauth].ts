6b876997c26c361de09f74f7f474c07531712742
Mateus oliveira <mthenriqueo@gmail.com>
2022-05-21 11:43:29 -0300

    Save logged user info on faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query } from 'faunadb';
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,39 @@ export default NextAuth({
       // scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (error) {
+        console.error('ERROR: ', error)
+        return false;
+      }
+      
+    }
+  }
 })
